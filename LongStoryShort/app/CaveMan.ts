1c7b3352a3efae660aa5c4b98c5e00697bf3c30a
Dia6lo <Dgamjm@gmail.com>
2016-09-22 14:49:12 +1000

    Complete caveman implementation.

--- LongStoryShort/app/CaveMan.ts
+++ LongStoryShort/app/CaveMan.ts
@@ -1,18 +1,85 @@
 ﻿class CaveMan {
-    private texture1 = Texture.fromImage("assets/CaveManMenu0.png");
-    private texture2 = Texture.fromImage("assets/CaveManMenu1.png");
-    sprite: Sprite;
+    private textures = [0, 1, 2, 3].map((i) => Texture.fromImage(`assets/CaveManMenu${i}.png`));
+    sprite = new Sprite(this.textures[0]);
 
     constructor() {
-        this.sprite = new Sprite(this.texture1);
-        const animation = new Animation(40);
-        animation.finalAction = Animation.loop();
-        const textureAnimator = Sprite.textureAnimator();
-        textureAnimator.setFrame(0, this.texture1);
-        textureAnimator.setFrame(20, this.texture2);
-        animation.setAnimator(textureAnimator);
-        this.sprite.animations.set("Idle", animation);
-        this.sprite.runAnimation("Idle");
         this.sprite.size = new Vector2(250, 250);
+
+        const idleAnimation = new Animation(40);
+        idleAnimation.finalAction = Animation.loop();
+        let textureAnimator = Sprite.textureAnimator();
+        textureAnimator.setFrame(0, this.textures[0]);
+        textureAnimator.setFrame(20, this.textures[1]);
+        idleAnimation.setAnimator(textureAnimator);
+        this.sprite.animations.set("Idle", idleAnimation);
+
+        const noticeAnimation = new Animation(1);
+        textureAnimator = Sprite.textureAnimator();
+        textureAnimator.setFrame(0, this.textures[2]);
+        noticeAnimation.setAnimator(textureAnimator);
+        this.sprite.animations.set("Notice", noticeAnimation);
+
+        const sadAnimation = new Animation(1);
+        textureAnimator = Sprite.textureAnimator();
+        textureAnimator.setFrame(0, this.textures[3]);
+        sadAnimation.setAnimator(textureAnimator);
+        this.sprite.animations.set("Sad", sadAnimation);
+
+        this.sprite.runAnimation("Idle");
+    }
+
+    runIdleAnimation() {
+        this.sprite.runAnimation("Idle");
+    }
+
+    runNoticeAnimation() {
+        this.sprite.runAnimation("Notice");
+    }
+
+    runSadAnimation() {
+        this.sprite.runAnimation("Sad");
+    }
+}
+
+class Campfire {
+    private textures = [0, 1, 2, 3].map((i) => Texture.fromImage(`assets/CaveManMenu${i}.png`));
+    sprite = new Sprite(this.textures[0]);
+
+    constructor() {
+        this.sprite.size = new Vector2(250, 250);
+
+        const idleAnimation = new Animation(40);
+        idleAnimation.finalAction = Animation.loop();
+        let textureAnimator = Sprite.textureAnimator();
+        textureAnimator.setFrame(0, this.textures[0]);
+        textureAnimator.setFrame(20, this.textures[1]);
+        idleAnimation.setAnimator(textureAnimator);
+        this.sprite.animations.set("Idle", idleAnimation);
+
+        const noticeAnimation = new Animation(1);
+        textureAnimator = Sprite.textureAnimator();
+        textureAnimator.setFrame(0, this.textures[2]);
+        noticeAnimation.setAnimator(textureAnimator);
+        this.sprite.animations.set("Notice", noticeAnimation);
+
+        const sadAnimation = new Animation(1);
+        textureAnimator = Sprite.textureAnimator();
+        textureAnimator.setFrame(0, this.textures[3]);
+        sadAnimation.setAnimator(textureAnimator);
+        this.sprite.animations.set("Sad", sadAnimation);
+
+        this.sprite.runAnimation("Idle");
+    }
+
+    runIdleAnimation() {
+        this.sprite.runAnimation("Idle");
+    }
+
+    runNoticeAnimation() {
+        this.sprite.runAnimation("Notice");
+    }
+
+    runSadAnimation() {
+        this.sprite.runAnimation("Sad");
     }
 }
